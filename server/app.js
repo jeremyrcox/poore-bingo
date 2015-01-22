84cf279e5dcca85f13b20c61423738b386fe4d13
Jeremy Cox <jeremyrcox.823@gmail.com>
2015-01-21 22:25:31 -0500

    added access endpoints to api

--- server/app.js
+++ server/app.js
@@ -2,6 +2,9 @@ var Hapi = require('hapi');
 var joi = require('joi');
 var hostname = require('os').hostname();
 var shuffle = require('shuffle-array');
+var bcrypt = require('bcrypt');
+var uuid = require('uuid');
+var db;
 
 var dbOpts = {
 	"url": "mongodb://localhost:27017/pooreBingo",
@@ -31,8 +34,8 @@ server.route({
     method: 'GET',
     path: '/game',
     handler: function (request, reply) {
-        var db = request.server.plugins['hapi-mongodb'].db;
-		
+    	db = request.server.plugins['hapi-mongodb'].db;
+
 		db.collection('words').find().toArray(function (err, doc){
 			reply(shuffle.pick(doc, 25));
 		});
@@ -43,8 +46,8 @@ server.route({
     method: 'GET',
     path: '/words',
     handler: function (request, reply) {
-        var db = request.server.plugins['hapi-mongodb'].db;
-		
+    	db = request.server.plugins['hapi-mongodb'].db;
+
 		db.collection('words').find().toArray(function (err, doc){
 			reply(doc);
 		});
@@ -59,8 +62,9 @@ server.route({
 			var newWord = {
 				word: request.payload.word,
 			};
+
+			db = request.server.plugins['hapi-mongodb'].db;
 			
-			var db = request.server.plugins['hapi-mongodb'].db;
 			db.collection('words').insert(newWord, {w:1}, function (err, doc){
 				if (err){
 					return reply(Hapi.error.internal('Internal MongoDB error', err));
@@ -78,6 +82,130 @@ server.route({
 	}
 });
 
+//User access
+server.route({
+	method: 'POST',
+	path: '/access/register',
+	config: {
+		handler: function(request, reply){
+			var email = request.payload.email;
+			var password = request.payload.password;
+			var userInfo = request.payload.info;
+			delete(userInfo.email);
+			delete(userInfo.password);
+
+			bcrypt.genSalt(10, function(err, salt){
+				bcrypt.hash(password, salt, function(err, hash){
+					if (err){
+						return reply(Hapi.error.internal('Error registering', err));
+					}else{
+						db = request.server.plugins['hapi-mongodb'].db;
+
+						db.collection('users').count({email: email}, function(err, count){
+							if (err){
+								return reply(Hapi.error.internal('Internal MongoDB error', err));
+							}else{
+								if(count > 0){
+									return reply(Hapi.error.badRequest('User already exists'));
+								}
+							}
+						});
+
+						var newUser = {
+							email: email,
+							password: hash,
+							info: userInfo
+						};
+
+						db.collection('users').insert(newUser, function (err, doc){
+							if (err){
+								return reply(Hapi.error.internal('Internal MongoDB error', err));
+							}else{
+								delete(doc.password);
+								reply(doc);
+							}
+						});
+					}
+				});
+			});
+		},
+
+		validate: {
+			payload: {
+				email: joi.string().email().required(),
+				password: joi.string().required(),
+				info: joi.object()
+			}
+		}
+	}
+});
+
+server.route({
+	method: 'POST',
+	path: '/access/login',
+	config: {
+		handler: function(request, reply){
+			var email = request.payload.email;
+			var password = request.payload.password;
+
+			db = request.server.plugins['hapi-mongodb'].db;
+
+			db.collection('users').findOne({email: email}, function(err, user){
+				if(err){
+					return reply(Hapi.error.internal('Internal MongoDB error', err));
+				}else{
+					bcrypt.compare(password, user.password, function(err, res){
+						if(res){
+							var token = uuid.v1();
+
+							db.collection('users').update({email: email}, {$set: {'token' : token}}, function(err, response){
+								if(err){
+									return reply(Hapi.error.internal('Internal MongoDB error', err));
+								}else{
+									user.token = token;
+									delete(user.password);
+									reply(user);
+								}
+							});
+						}else{
+							return reply(Hapi.error.forbidden());
+						}
+					});
+				}
+			});
+		},
+
+		validate: {
+			payload: {
+				email: joi.string().required(),
+				password: joi.string().required()
+			}
+		}
+	}
+});
+
+server.route({
+	method: 'POST',
+	path: '/access/logout',
+	config: {
+		handler: function(request, reply){
+			var email = request.payload.email;
+			var token = request.payload.token;
+
+			db = request.server.plugins['hapi-mongodb'].db;
+
+			db.collection('users').update({email: email, token: token}, {token: null}, function(err, response){
+				if(err){
+					return reply(Hapi.error.internal('Internal MongoDB error', err));
+				}else{
+					reply(response);
+				}
+			});
+		}
+	}
+})
+
+//Basic file response
 server.route({
     method: 'GET',
     path: '/{param*}',
